b2a693b9cc49897083f96560872b5a8b1e65ca50
Luis Quevedo <qfluis@gmail.com>
2022-04-19 15:12:01 +0200

    Ejercicio publisher/subscriber revisado

--- 3_publisher_subscriber/cliente.js
+++ 3_publisher_subscriber/cliente.js
@@ -32,32 +32,3 @@ class Subscriber {
 
 const subscriber = new Subscriber("Futurama", "amqp://localhost");
 subscriber.subscribe();
-/*
-const amqp = require('amqplib/callback_api');
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
-
-        var queue = 'hello';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-        
-        const consoleColor = "\x1b[36m%s\x1b[0m"; // \x1b[34m
-
-        console.log(consoleColor,`CLIENT > Waiting for messages in ${queue}. To exit press CTRL+C`);
-
-        channel.consume(queue, function(msg) {
-            console.log(consoleColor, `CLIENT > Received ${msg.content.toString()}`);
-        }, {
-            noAck: true
-        });
-    });
-});
-*/
--- 3_publisher_subscriber/servidor.js
+++ 3_publisher_subscriber/servidor.js
@@ -27,7 +27,7 @@ class Publisher {
             setTimeout(function() {
                 connection.close();
                 process.exit(0)
-            }, 10000);
+            }, 1000);
         });        
     }
 }
@@ -36,37 +36,3 @@ publisher = new Publisher("Futurama", "amqp://localhost")
 publisher.sendMsg("Holiwi");
 setTimeout(() => publisher.sendMsg("de"), 600);
 setTimeout(() => publisher.sendMsg("Kiwi"), 800);
-
-
-
-/*
-const amqp = require('amqplib/callback_api');
-amqp.connect('amqp://localhost', (error0, connection) => {
-    if (error0){
-        throw error0;
-    }
-    connection.createChannel((error1, channel) => {
-        if (error1) {
-            throw error1;
-        }
-        var queue = 'hello';
-        var msg = 'Hello world';
-
-        channel.assertQueue(queue, {
-        durable: false
-        });
-
-        const consoleColor = "\x1b[35m%s\x1b[0m";        
-        console.log(consoleColor,`SERVER > Start queue ${queue}`);
-
-        channel.sendToQueue(queue, Buffer.from(msg));       
-
-        console.log(consoleColor,`SERVER > Sent > ${msg}`);
-    });
-
-    setTimeout(function() {
-        connection.close();
-        process.exit(0)
-    }, 50000);
-});
-*/
